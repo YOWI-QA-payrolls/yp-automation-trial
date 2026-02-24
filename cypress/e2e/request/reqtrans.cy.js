e71aad6313a21543cd1076086063cb97e5dc76f6
BweepBwop <a.kelsey.magat@gmail.com>
2026-02-24 11:29:40 +0800

    selecting an employee fix

--- cypress/e2e/request/reqtrans.cy.js
+++ cypress/e2e/request/reqtrans.cy.js
@@ -46,8 +46,8 @@ describe('Request - Transfer', () => {
                 cy.wait(1000);
                 cy.select2First(':nth-child(5) > .ui-select-container > .select2-choice > .select2-arrow > b');
                 cy.wait(1000);
-                cy.get('.select2-choices', { timeout: 10000 }).first().click({ force: true });
-                cy.get('.select2-results li', { timeout: 10000 }).first().click({ force: true });
+                cy.get('.select2-choices .select2-input', { timeout: 10000 }).first().click({ force: true }).type('aria');
+                cy.get('.ui-select-choices-row', { timeout: 10000 }).first().click({ force: true });
                 cy.wait(500);
 
                 cy.wait(1000);
