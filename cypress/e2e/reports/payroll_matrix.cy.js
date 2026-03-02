293327c1f0bbd9c72b537514427c50b8ecf581d5
BweepBwop <a.kelsey.magat@gmail.com>
2026-03-02 15:48:19 +0800

    fix: use exact ng-click selectors for funding and deposit date pickers in payroll matrix test

--- cypress/e2e/reports/payroll_matrix.cy.js
+++ cypress/e2e/reports/payroll_matrix.cy.js
@@ -7,7 +7,28 @@ describe('Reports - Payroll Summary Matrix', () => {
     it('should select payroll and search matrix', () => {
         cy.navigateMenu(['#reports_list > [href="#"]', '#payroll_summary_matrix > a']);
         cy.wait(1000);
-        cy.select2First('.select2-choices');
-        cy.get('#advance-search', { timeout: 15000 }).should('be.visible').click();
+        cy.get('.select2-choices input.select2-input', { timeout: 10000 })
+            .first()
+            .click({ force: true });
+        cy.get('.ui-select-choices-row-inner', { timeout: 15000 })
+            .should('be.visible')
+            .first()
+            .click({ force: true });
+
+        cy.get('[ng-click="main.open_date(\'bugo_funding_date\')"]', { timeout: 10000 })
+            .should('be.visible')
+            .click();
+        cy.get('.uib-datepicker-popup', { timeout: 10000 })
+            .should('be.visible')
+            .contains('button', '1')
+            .click();
+
+        cy.get('[ng-click="main.open_date(\'bugo_deposit_date\')"]', { timeout: 10000 })
+            .should('be.visible')
+            .click();
+        cy.get('.uib-datepicker-popup', { timeout: 10000 })
+            .should('be.visible')
+            .contains('button', '1')
+            .click();
     });
 });
